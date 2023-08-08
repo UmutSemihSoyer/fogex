bebfb7f8b47b65b062c806ee7154fac333d8ff72
UmutSemihSoyer <umutsemihsoyer@hotmail.com>
2023-08-08 16:59:25 +0300

    test

--- fogex/patterns/isWhiteSpace.ts
+++ fogex/patterns/isWhiteSpace.ts
@@ -2,7 +2,10 @@ const pattern = new RegExp(/^\s*/)
 
 const isWhiteSpace = (value) => {
   if (value === null || pattern === new RegExp('/(?:)/')) return
-  return pattern.test(value)
+            return pattern.test(value)
 }
+
+
+
 
 export default isWhiteSpace
--- test/isBoolean.test.ts
+++ test/isBoolean.test.ts
@@ -3,4 +3,6 @@ import isBoolean from '../fogex/patterns/isBoolean';
 it('should return true for valid boolean values', () => {
   expect(isBoolean(true)).toBe(true);
   expect(isBoolean(false)).toBe(true);
+  expect(isBoolean(1)).toBe(false);
+  expect(isBoolean('string')).toBe(false);
 });
